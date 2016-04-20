ea9a6f638eefb5442de8716db26bb03676100eec
Matilda Pan <matilda.yipan@gmail.com>
2016-04-20 22:46:26 +1000

    add web worker and improve calculate pixel algorithm

--- js/client.js
+++ js/client.js
@@ -39,100 +39,26 @@ window.onload = function () {
 
 };
 
+var w;
+
+function StartGeneration() {
+    if (typeof (Worker) !== "undefined") {
+        w = new Worker("js/myWorker.js");
+        let uploaded_image = document.getElementById("uploadedImage")
+        let ctx = uploaded_image.getContext('2d');
+        var data = ctx.getImageData(0, 0, uploaded_image.width, uploaded_image.height).data;
+        w.postMessage({totalPixelData:data, width:uploaded_image.width, height:uploaded_image.height});
+        w.onmessage = function (event) {
+            let tiles_div = document.createElement("div");
+            tiles_div.className = "tiles";
+            document.getElementsByTagName("body")[0].appendChild(tiles_div);
+            tiles_div.innerHTML = event.data;
+        };
+    } else {
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Workers...";
+    }
+}
+
 // function showUploadedImage(e) {
 //     document.getElementById("myImage").setAttribute('src', e.target.result);
 // }
-
-function MosaicGenerator() {
- 
-    var uploaded_image = document.getElementById("uploadedImage");
-    var image_width = uploaded_image.width;
-    var image_height = uploaded_image.height;
-
-
-    var num = 10;
-    var tile_width = image_width/num;
-    var tile_height = image_height/num;
-    
-
-    pieces = [];
-    for(var i = 0; i <num; i++){
-        for(var j = 0; j <num; j++){
-            var p = {col: i, row: j};
-            pieces.push(p);
-        }
-    }
-  
-        
-    for(var index=0; index<pieces.length; index++){
-        
-        var p = pieces[index];
-        var row = p.row;
-        var col = p.col;
-        
-        var colorSum = { r:0, g:0, b:0 }
-        var count = tile_width * tile_height;
-        for (var i = row*tile_width; i < (row+1)*tile_width; i++){
-            for(var j=col*tile_height; j < (col+1)*tile_height; j++) {
-                var pixelData = uploaded_image.getContext('2d').getImageData(i, j, 1, 1).data;
-                colorSum.r += pixelData[0];
-                colorSum.g += pixelData[1];
-                colorSum.b += pixelData[2];
-                //colorSum.a += pixelData[3];
-            } 
-        }
-        
-        var averageColor = {};
-        averageColor.r = Math.round(getAverageColor(colorSum.r, count));
-        averageColor.g = Math.round(getAverageColor(colorSum.g, count));
-        averageColor.b = Math.round(getAverageColor(colorSum.b, count));
-        //averageColor.a = colorSum.a/count;
-        
-        var averageColorHex = rgbToHex(averageColor.r, averageColor.g, averageColor.b);
-        
-        p.averageColorHex = averageColorHex;
-        
-        //var svg = sendHexColor(averageColorHex);
-        
-        p.xhttp = new XMLHttpRequest();
-        p.xhttp.open("GET", "/color/" + averageColorHex, true);
-        p.xhttp.send();
-        
-        p.xhttp.addEventListener("readystatechange", processRequest, false);
-        p.processed = false;
-     
-    }
-      
-}
-
-
-function processRequest(e) {
-    
-    for(var i=0; i<pieces.length; i++){
-        var p = pieces[i];
-        if (p.xhttp.readyState == 4 && p.xhttp.status == 200 && !p.processed) {
-
-            p.processed = true;
-            
-            var tiles_div = document.createElement("div");
-            tiles_div.className = "tiles"; 
-            document.getElementsByTagName("body")[0].appendChild(tiles_div);
-            var svg = p.xhttp.responseText;
-            tiles_div.innerHTML = svg;
-        }
-    }
-    
-}
-
-function getAverageColor(color_sum, count) {
-    return color_sum/count;
-}
-
-function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-}
-
-function rgbToHex(r, g, b) {
-    return componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
---
+++ js/myWorker.js
@@ -0,0 +1,129 @@
+
+var imageLoaded = {};
+var loadedRow = [];
+var pieces = [];
+
+onmessage = function(e) {
+    console.log('Message received from main script');
+     MosaicGenerator(e.data.totalPixelData, e.data.width, e.data.height);
+}
+
+function MosaicGenerator(totalPixelData, width, height) {
+ 
+    //var uploaded_image = document.getElementById("uploadedImage");
+    var image_width = width;
+    var image_height = height;
+
+
+    var num = 50;
+    var tile_width = image_width/num;
+    var tile_height = image_height/num;
+    
+
+    for(var i = 0; i <num; i++){
+        for(var j = 0; j <num; j++){
+            var p = {col: i, row: j};
+            pieces.push(p);
+        }
+    }
+  
+        
+    for(var index=0; index<pieces.length; index++){
+        
+     
+        
+        let p = pieces[index];
+        
+       
+        let row = p.row;
+        let col = p.col;
+        
+        let colorSum = { r:0, g:0, b:0 }
+        let count = tile_width * tile_height;
+        for (let i = row*tile_width; i < (row+1)*tile_width; i++){
+            for(let j=col*tile_height; j < (col+1)*tile_height; j++) {
+                
+                let offset = (i * image_width + j) * 4;
+                
+                colorSum.r += totalPixelData[offset + 0];
+                colorSum.g += totalPixelData[offset + 1];
+                colorSum.b += totalPixelData[offset + 2];
+                //colorSum.a += pixelData[offset + 3];
+            } 
+        }
+        
+        let averageColor = {};
+        averageColor.r = Math.round(getAverageColor(colorSum.r, count));
+        averageColor.g = Math.round(getAverageColor(colorSum.g, count));
+        averageColor.b = Math.round(getAverageColor(colorSum.b, count));
+        //averageColor.a = colorSum.a/count;
+        
+        let averageColorHex = rgbToHex(averageColor.r, averageColor.g, averageColor.b);
+        
+        p.averageColorHex = averageColorHex;
+        
+        
+        if(!imageLoaded[p.row]){
+            imageLoaded[p.row] = {};
+            imageLoaded[p.row].Count = 0;
+            imageLoaded[p.row].Images = new Array(num);
+            imageLoaded[p.row].showed = false;
+            for (let k = 0; k < num; k++){
+                imageLoaded[p.row].Images[k] = '';
+            }        
+        }
+       // postMessage("Tile "+p.row+","+p.col+" processed.")
+        makeRequest("/color/" + averageColorHex).then(function(svg){
+            imageLoaded[p.row].Count += 1; 
+            imageLoaded[p.row].Images[p.col] += svg; 
+            if (imageLoaded[p.row].Count == num && imageLoaded[p.row].showed == false){
+                let rowImgaes = imageLoaded[p.row].Images.join('');
+                postMessage(rowImgaes);
+                imageLoaded[p.row].showed == true;
+            }
+            
+        },function(error){
+            console.log(error.status + ": "+error.statusText);
+        })
+     
+    }
+      
+}
+
+function makeRequest(url) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = function () {
+            if (this.status == 200) {
+                resolve(xhr.response);
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
+}
+
+
+function getAverageColor(color_sum, count) {
+    return color_sum/count;
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
